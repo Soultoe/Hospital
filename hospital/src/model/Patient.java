4bf48d54d73d390d03fdd24338654a2b25b87067
RomainB <romainbrisse@live.fr>
2018-04-07 19:16:06 +0200

    Patient class

---
+++ hospital/src/model/Patient.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author romain
+ */
+public class Patient extends Person {
+    
+    private final int socialSecurityNumber;
+    
+    /**
+     *@name Default constructor
+     */
+    public Patient()
+    {
+        super();
+        this.socialSecurityNumber = 0;
+    }
+    
+    /**
+    * @name Overload constructor
+    * @param id
+    * @param name
+    * @param surname
+    * @param address
+    * @param phone 
+    * @param socialSecurityNumber
+    */
+    public Patient(int id, String name, String surname, String address, String phone, int socialSecurityNumber)
+    {
+        super(id,name,surname,address,phone);
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+    
+}
