c121bea8815074a353e0b906d2d3312e24e21327
LENOVOW530\Ana <petitpetitevy@gmail.com>
2018-04-08 22:02:24 +0200

    Ajout connexion distant
    Déso j'ai eu la flemme de changer tous mes trucs.
    Oublie pas de te co en même temps à Putty pour essayer

--- hospital/src/hospital/Hospital.java
+++ hospital/src/hospital/Hospital.java
@@ -18,26 +18,25 @@ public class Hospital {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        //ma tentative de connexion tunnel SSH T-T 
-        //j'ai demandé à Gab
-        //String username = "ad151913";
-        //String password = "mvkPTN~0109";
-        //String firstHost = "gandalf.ece.fr";
-        //String secondHost = "sql-users.ece.fr";
-        //int firstHostPort = 22;
-        //int secondHostPort = 3305;
-        
-        
-        // SSHTunnel con1 = new SSHTunnel(username,password,firstHost,secondHost,firstHostPort,secondHostPort);
-        //con1.connect();
+ 
+        //connexion en distant
+        String usernameECE = "ad151913";
+        String passwordECE = "mdp";
+        String loginDatabaseECE = "ad151913-rw";
+        String passwordDatabaseECE = "tNi6Vlia";
         
         //connexion en local
         String nameDatabase = "Hopital";
         String loginDatabase = "root";
         String passwordDatabase = "";   
-                
+          
+        
         try
         {
+            //distant
+            //Connexion con1 = new Connexion(usernameECE, passwordECE, loginDatabaseECE, passwordDatabaseECE);
+            
+            //local
             Connexion con1 = new Connexion(nameDatabase, loginDatabase, passwordDatabase);
             System.out.println(con1.remplirChampsRequete("SELECT * from malade"));
 
