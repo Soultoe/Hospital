e8723e4ca08d79c43bccb040d9174ecd7d24154f
LENOVOW530\Ana <petitpetitevy@gmail.com>
2018-04-09 22:51:06 +0200

    Début test plusieurs requêtes select
    Fais toujours pas gaffe c'est moi qui fais mes tests

--- hospital/src/hospital/Hospital.java
+++ hospital/src/hospital/Hospital.java
@@ -7,6 +7,7 @@
 package hospital;
 import model.*;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 /**
  *
@@ -29,8 +30,12 @@ public class Hospital {
         String nameDatabase = "Hopital";
         String loginDatabase = "root";
         String passwordDatabase = "";   
+                  
           
-        
+        //pour mon test
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Que voulez vous chercher? Patients selon leur mutuelle (0)), rotation des infirmiers (1)");
+        int choice = sc.nextInt();
         try
         {
             //distant
@@ -39,7 +44,17 @@ public class Hospital {
             //local
             Connexion con1 = new Connexion(nameDatabase, loginDatabase, passwordDatabase);
             Search SearchMutual = new Search(con1);
-            SearchMutual.SearchWithWhere("nom,prenom","malade",SearchMutual.SearchMutual());
+            
+            //c'est juste un teste hein je nettoierai ça après
+            if(choice == 0)
+            {
+                SearchMutual.SearchWithWhere("nom,prenom","malade",SearchMutual.SearchMutual());
+            }
+            if(choice == 1)
+            {
+                SearchMutual.SearchWithWhere("numero","infirmier",SearchMutual.nursingRotation());
+            }
+            
 
         }
         catch(Exception e)
--- hospital/src/model/Search.java
+++ hospital/src/model/Search.java
@@ -30,11 +30,31 @@ public class Search {
         System.out.println(con.remplirChampsRequete("SELECT "+select+" FROM "+from+" WHERE "+where));
     }
     
+    //Faudra un champ texte pour celui là
     public String SearchMutual()
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Selon quelle mutuelle voulez vous trier les patients?");
         String MutualName = sc.nextLine();
         return "mutuelle like '"+MutualName+"'";
+    }
+    
+    //des boutons suffisent pour celui-là
+    public String nursingRotation()
+    {
+        Scanner sc = new Scanner(System.in);
+        int rotation;
+        do
+        {
+            System.out.println("Infirmiers travaillant le jour (0) ou la nuit (1)");
+            rotation = sc.nextInt();
+        }while((rotation != 1)&&(rotation != 0));
+        
+        if(rotation == 0)
+            return "rotation like 'NUIT'";
+        else
+            return "rotation like 'JOUR'";
+        
+        
     }
 }
